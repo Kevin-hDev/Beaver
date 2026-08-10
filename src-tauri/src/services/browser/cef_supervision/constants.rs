08a3c7d102d2119b60e204a4787a7848549b0228
kevin-hDev <huynh.kevin7@outlook.fr>
2026-08-10 14:12:24 +0200

    feat(browser): add private CEF admission authority

--- src-tauri/src/services/browser/cef_supervision/constants.rs
+++ src-tauri/src/services/browser/cef_supervision/constants.rs
@@ -1,3 +1,4 @@
 pub(in crate::services::browser) const CEF_SLOT_CAPACITY: usize = 64;
 pub(super) const CEF_MARKER_MAX_BYTES: usize = 128;
 pub(super) const CEF_NONCE_BYTES: usize = 32;
+pub(super) const GATE_RECHECK: std::time::Duration = std::time::Duration::from_millis(1);
