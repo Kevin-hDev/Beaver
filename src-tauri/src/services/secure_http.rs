0f61ccbe25cb73bde308c8b1f23d91ca4e172d21
Kevin Huynh <huynh.kevin7@outlook.fr>
2026-09-21 04:12:36 +0200

    fix(ci): compile HTTP DELETE wrapper only where used

--- src-tauri/src/services/secure_http.rs
+++ src-tauri/src/services/secure_http.rs
@@ -101,6 +101,7 @@ impl AuthenticatedClient {
         self.client.post(url)
     }
 
+    #[cfg(any(target_os = "macos", target_os = "windows", test))]
     pub fn delete<U: IntoUrl>(&self, url: U) -> RequestBuilder {
         self.client.delete(url)
     }
